9548c6a03b9ae51372336270cb9c71bbb5b8d903
zhdds <zhddsj@qq.com>
2026-04-12 16:14:19 +0800

    添加方块迭代方向调整功能，用于边境堡垒全怪

--- src/main/java/org/carpet/sgu/SguSettings.java
+++ src/main/java/org/carpet/sgu/SguSettings.java
@@ -5,9 +5,19 @@ import carpet.api.settings.Rule;
 import static carpet.api.settings.RuleCategory.EXPERIMENTAL;
 import static carpet.api.settings.RuleCategory.FEATURE;
 
+
+
 public class SguSettings {
+
+    public static final String SGU = "SGU";
+
     @Rule(
-            categories = {FEATURE, EXPERIMENTAL}
+            categories = {FEATURE, EXPERIMENTAL,SGU}
     )
     public static boolean betterFakePlayerProcess = false;
+
+    @Rule(
+            categories = {FEATURE, EXPERIMENTAL,SGU}
+    )
+    public static boolean reverseBlockPosTraversal = false;
 }
