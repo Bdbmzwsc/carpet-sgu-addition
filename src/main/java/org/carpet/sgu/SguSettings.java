fe8e8df3b77961bb9804e2932b340b93796adada
Bdbmzwsc <chinabdbmzwsc@outlook.com>
2026-04-17 22:38:51 +0800

    feat: added SGU category

--- src/main/java/org/carpet/sgu/SguSettings.java
+++ src/main/java/org/carpet/sgu/SguSettings.java
@@ -6,8 +6,10 @@ import static carpet.api.settings.RuleCategory.EXPERIMENTAL;
 import static carpet.api.settings.RuleCategory.FEATURE;
 
 public class SguSettings {
+    public static final String SGU = "sgu_addition";
+
     @Rule(
-            categories = {FEATURE, EXPERIMENTAL}
+            categories = {SGU, FEATURE, EXPERIMENTAL}
     )
     public static boolean betterFakePlayerProcess = false;
 }
